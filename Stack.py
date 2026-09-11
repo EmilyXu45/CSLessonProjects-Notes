15aa6eeff73c49878d9296947656f53dd8862160
EmilyXu45 <emilyxu030@gmail.com>
2026-09-11 20:48:20 +0800

    Push procedure

--- Stack.py
+++ Stack.py
@@ -3,3 +3,11 @@ BasePointer = 0 #0 because the list starts on index 0, this is the bottom of the
 TopPointer = BasePointer - 1 # The stack is initially empty, when the first item is pushed in, the TopPointer will move from -1 to 0, which will point to the first item
 StackFull = len(Stack)-1 # This indicates the last available position (NOT the size of the stack)
 
+def push(item):
+    global TopPointer, stackFull
+    if TopPointer < stackFull: # The stack still has space; the last position has not been reached
+        Stack[TopPointer + 1]= item # Add the data
+        TopPointer += 1 # Update the pointer
+        print (item, "pushed")
+    else:
+        print ("Stack is full, cannot push")

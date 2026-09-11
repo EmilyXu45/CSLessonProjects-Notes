c9205f6b19bca2472b7456fd30df33d1033a08b2
EmilyXu45 <emilyxu030@gmail.com>
2026-09-11 20:58:33 +0800

    Added test data

--- Stack.py
+++ Stack.py
@@ -4,8 +4,8 @@ TopPointer = BasePointer - 1 # The stack is initially empty, when the first item
 StackFull = len(Stack)-1 # This indicates the last available position (NOT the size of the stack)
 
 def push(item):
-    global TopPointer, stackFull
-    if TopPointer < stackFull: # The stack still has space; the last position has not been reached
+    global TopPointer, StackFull
+    if TopPointer < StackFull: # The stack still has space; the last position has not been reached
         Stack[TopPointer + 1]= item # Add the data
         TopPointer += 1 # Update the pointer
         print (item, "pushed")
@@ -22,3 +22,8 @@ def pop():
     else:
         print("Stack is empty, cannot pop")
 
+for i in range (10):
+    push(i)
+pop()
+pop()
+print(Stack)

8ce115057126a2f3fba28ec7598c7f51883cf9e6
EmilyXu45 <emilyxu030@gmail.com>
2026-09-18 18:16:33 +0800

    loop

---
+++ Dictionaries.py
@@ -0,0 +1,23 @@
+# Dictionary = {"Key": "Value"}
+# Dictionary keys are unique and immutable; they can be a string, number or a tuple (lists are mutable)
+# Values are more flexible and can be a string, number, list or another dictionary
+capitals = {"USA":"Washington D.C.", "France":"Paris"}
+# Add key-value pair
+capitals = {"Japan":"Tokyo"}
+
+things = {
+    # "Key": ["Value as a list"]
+    "Fruit": ["Mango", "Banana"],
+    "Color": "Yellow"
+}
+
+# get() can be used to return a default message when the key is not found; if the key is found, the value is returned
+capitals = {"USA":"Washington D.C.", "France":"Paris", "India":"New Delhi"}
+
+country = input("Country: ")
+capital = capitals.get(country, "Unknown country")
+print(capital)
+
+# loop through the dictionary so that each country and its capital is printed.
+for country in capitals:
+    print(country, "->", capitals[country])
